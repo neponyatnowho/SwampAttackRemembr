b2f942571fefe91552d992add76cc46a53f41e99
neponyatno_who <romanafksher@gmail.com>
2022-11-14 15:06:59 -0800

    heath bar ui

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 [RequireComponent(typeof(Animator))]
 public class Player : MonoBehaviour
@@ -14,6 +15,8 @@ public class Player : MonoBehaviour
     private Animator _animator;
 
     public int Money {get; private set;}
+
+    public event UnityAction<int,int> HealthChanged;
 
     private void Start()
     {
@@ -33,6 +36,7 @@ public class Player : MonoBehaviour
     public void ApplayDamage(int damage)
     {
         _currentHealth -= damage;
+        HealthChanged?.Invoke(_currentHealth, _health);
 
         if (_currentHealth <= 0)
             Destroy(gameObject);
